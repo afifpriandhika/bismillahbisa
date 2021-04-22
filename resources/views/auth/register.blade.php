68d8a847faac5b62c3e46417c42e6ce510a96a9b
GINNA TRIGAMA <ginnatrigama@gmail.com>
2021-04-22 15:17:45 +0700

    registrasi

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,17 +2,17 @@
 
 @section('main-content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-xl-10 col-lg-12 col-md-9">
-            <div class="card o-hidden border-0 shadow-lg my-5">
-                <div class="card-body p-0">
-                    <div class="row">
-                        <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                        <div class="col-lg-6">
-                            <div class="p-5">
+    <div class="row">
+        <div class="col-6 col-md-4"></div>
+        <div class="col-6 col-md-4"> <br>
+                
+                <div class="card-body">
                                 <div class="text-center">
-                                    <h1 class="h4 text-gray-900 mb-4">{{ __('Register') }}</h1>
+                                    <h1 class="h3 text-gray-900 mb-4"><b>{{ __('Daftar') }}</b></h1>
                                 </div>
+                                    <hr width="25%" style="border: solid #F57ABC ">
+                                
+                                    <p class="text-center" style="color: black;">Daftarkan dirimu & bagi pengalamanmu bersama kami</p>
 
                                 @if ($errors->any())
                                     <div class="alert alert-danger border-left-danger" role="alert">
@@ -28,37 +28,34 @@
                                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
                                     <div class="form-group">
-                                        <input type="text" class="form-control form-control-user" name="name" placeholder="{{ __('Name') }}" value="{{ old('name') }}" required autofocus>
+                                        <input type="text" class="form-control col" name="name" placeholder="{{ __('Nama') }}" value="{{ old('name') }}" required autofocus>
                                     </div>
 
                                     <div class="form-group">
-                                        <input type="text" class="form-control form-control-user" name="last_name" placeholder="{{ __('Last Name') }}" value="{{ old('last_name') }}" required>
+                                        <input type="text" class="form-control col" name="username" placeholder="{{ __('Username') }}" value="{{ old('last_name') }}" required>
                                     </div>
 
                                     <div class="form-group">
-                                        <input type="email" class="form-control form-control-user" name="email" placeholder="{{ __('E-Mail Address') }}" value="{{ old('email') }}" required>
+                                        <input type="email" class="form-control col" name="email" placeholder="{{ __('E-Mail') }}" value="{{ old('email') }}" required>
                                     </div>
 
                                     <div class="form-group">
-                                        <input type="password" class="form-control form-control-user" name="password" placeholder="{{ __('Password') }}" required>
+                                        <input type="password" class="form-control col" name="password" placeholder="{{ __('Masukkan Password') }}" required>
                                     </div>
 
-                                    <div class="form-group">
-                                        <input type="password" class="form-control form-control-user" name="password_confirmation" placeholder="{{ __('Confirm Password') }}" required>
-                                    </div>
 
                                     <div class="form-group">
-                                        <button type="submit" class="btn btn-primary btn-user btn-block">
-                                            {{ __('Register') }}
+                                        <button type="submit" class="btn col" class="btn col" style="background-color: #F57ABC; color: white; box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);">
+                                            {{ __('Daftar Sekarang') }}
                                         </button>
                                     </div>
                                 </form>
 
-                                <hr>
+                               
 
                                 <div class="text-center">
-                                    <a class="small" href="{{ route('login') }}">
-                                        {{ __('Already have an account? Login!') }}
+                                    <a class="small" style="color: #F57ABC" href="{{ route('login') }}">
+                                        {{ __('Sudah Memiliki Akun? Login!') }}
                                     </a>
                                 </div>
                             </div>
